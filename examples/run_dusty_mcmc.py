4938ba3ab2d93bc2e8a67d9315558f559e482801
viraj21197 <karambelkarvraj21197@gmail.com>
2022-12-04 09:33:52 -0800

    add parallel processing

--- examples/run_dusty_mcmc.py
+++ examples/run_dusty_mcmc.py
@@ -1,10 +1,14 @@
 from pydusty.dusty import Dusty, DustyParameters
 from pydusty.mcmc import Emcee
 from pydusty.parameters import Parameter
-from pydusty.priors import GaussianPrior, UniformPrior
+from pydusty.priors import UniformPrior
 from pydusty.utils import get_default_argparser, load_and_extcor_data, getLogger
-import logging
-import sys
+from multiprocessing import Pool
+from datetime import datetime
+
+
+def run_mcmc_process(emcee_runner: Emcee):
+    emcee_runner.run_mcmc()
 
 
 if __name__ == '__main__':
@@ -42,39 +46,50 @@ if __name__ == '__main__':
     custom_grain_distribution = Parameter(name='custom_grain_distribution',
                                           value=False)
 
-    dusty_parameters = DustyParameters(
-        tstar=tstar,
-        tdust=tdust,
-        tau=tau,
-        blackbody=blackbody,
-        shell_thickness=shell_thickness,
-        dust_type=dust_type,
-        tstarmin=tstarmin,
-        tstarmax=tstarmax,
-        custom_grain_distribution=custom_grain_distribution
-    )
-
     working_dir = args.workdir
     if working_dir is None:
         working_dir = './run'
 
-    basic_dusty = Dusty(parameters=dusty_parameters,
-                        dusty_working_directory=working_dir)
+    nprocesses = args.nprocesses
+    logger.info(f"Creating {nprocesses} processes.")
+    pool = Pool(nprocesses)
 
-    ext_corrected_obsdata = load_and_extcor_data(args.object_photometry_file)
+    emcee_runners = []
+    for process_num in nprocesses:
+        dusty_parameters = DustyParameters(
+            tstar=tstar,
+            tdust=tdust,
+            tau=tau,
+            blackbody=blackbody,
+            shell_thickness=shell_thickness,
+            dust_type=dust_type,
+            tstarmin=tstarmin,
+            tstarmax=tstarmax,
+            custom_grain_distribution=custom_grain_distribution
+        )
 
-    emcee_runner = Emcee(obsdata=ext_corrected_obsdata,
-                         object_photometry_file=args.object_photometry_file,
-                         nwalkers=args.nwalkers,
-                         ntrials=args.ntrials,
-                         dusty=basic_dusty,
-                         correct_for_molecular_absorption=args.molecular_absorption,
-                         molecular_absorption_lookup_table_path=args.molecular_table_path,
-                         limits_only=args.limits_only,
-                         fixLstar=args.fixLstar,
-                         chi_square_limits_only=args.chi_square_limits_only,
-                         extrapolation=args.extrapolation,
-                         continue_from_file=args.continue_from_file,
-                         )
+        dusty_process_working_dir = working_dir + f'_{process_num}'
+        basic_dusty = Dusty(parameters=dusty_parameters,
+                            dusty_working_directory=dusty_process_working_dir,
+                            )
 
-    emcee_runner.run_mcmc()
+        ext_corrected_obsdata = load_and_extcor_data(args.object_photometry_file)
+
+        emcee_runner = Emcee(obsdata=ext_corrected_obsdata,
+                             object_photometry_file=args.object_photometry_file,
+                             nwalkers=args.nwalkers,
+                             ntrials=args.ntrials,
+                             dusty=basic_dusty,
+                             correct_for_molecular_absorption=args.molecular_absorption,
+                             molecular_absorption_lookup_table_path=args.molecular_table_path,
+                             limits_only=args.limits_only,
+                             fixLstar=args.fixLstar,
+                             chi_square_limits_only=args.chi_square_limits_only,
+                             extrapolation=args.extrapolation,
+                             continue_from_file=args.continue_from_file,
+                             random_seed=process_num
+                             )
+
+        emcee_runners.append(emcee_runner)
+
+    pool.map(run_mcmc_process, emcee_runners)
