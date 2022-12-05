8643ae1bbd9f3025647dc7f405068d919af6475e
viraj21197 <karambelkarvraj21197@gmail.com>
2022-12-05 13:10:45 -0800

    refactor

--- examples/run_dusty_mcmc.py
+++ examples/run_dusty_mcmc.py
@@ -19,18 +19,18 @@ if __name__ == '__main__':
 
     # Set initial parameters
     tstar = Parameter(name='tstar',
-                      value=4000,
-                      prior=UniformPrior(prior_parameters=(3000, 6000)),
+                      value=5000,
+                      prior=UniformPrior(prior_parameters=(4000, 7000)),
                       is_variable=True)
 
     tdust = Parameter(name='tdust',
-                      value=1000,
-                      prior=UniformPrior(prior_parameters=(500, 2000)),
+                      value=700,
+                      prior=UniformPrior(prior_parameters=(300, 1200)),
                       is_variable=True)
 
     tau = Parameter(name='tau',
                     value=1,
-                    prior=UniformPrior(prior_parameters=(0, 5)),
+                    prior=UniformPrior(prior_parameters=(1e-3, 7)),
                     is_variable=True)
 
     blackbody = Parameter(name='blackbody',
@@ -46,40 +46,40 @@ if __name__ == '__main__':
     custom_grain_distribution = Parameter(name='custom_grain_distribution',
                                           value=False)
 
-    working_dir = args.workdir
+    dusty_parameters = DustyParameters(
+        tstar=tstar,
+        tdust=tdust,
+        tau=tau,
+        blackbody=blackbody,
+        shell_thickness=shell_thickness,
+        dust_type=dust_type,
+        tstarmin=tstarmin,
+        tstarmax=tstarmax,
+        custom_grain_distribution=custom_grain_distribution
+    )
 
     nprocesses = args.nprocesses
     logger.info(f"Creating {nprocesses} processes.")
     pool = Pool(nprocesses)
     tstart = datetime.utcnow()
+    working_dir = args.workdir
+
+    ext_corrected_obsdata = load_and_extcor_data(args.object_photometry_file)
+
     emcee_runners = []
 
     for process_num in range(nprocesses):
-        dusty_parameters = DustyParameters(
-            tstar=tstar,
-            tdust=tdust,
-            tau=tau,
-            blackbody=blackbody,
-            shell_thickness=shell_thickness,
-            dust_type=dust_type,
-            tstarmin=tstarmin,
-            tstarmax=tstarmax,
-            custom_grain_distribution=custom_grain_distribution
-        )
-
         dusty_process_working_dir = working_dir + f'_{process_num}'
         basic_dusty = Dusty(parameters=dusty_parameters,
                             dusty_working_directory=dusty_process_working_dir,
                             dusty_file_directory=args.dusty_file_dir
                             )
 
-        ext_corrected_obsdata = load_and_extcor_data(args.object_photometry_file)
-
-        emcee_runner = Emcee(obsdata=ext_corrected_obsdata,
+        emcee_runner = Emcee(dusty=basic_dusty,
+                             obsdata=ext_corrected_obsdata,
                              object_photometry_file=args.object_photometry_file,
                              nwalkers=args.nwalkers,
                              ntrials=int(args.ntrials/nprocesses),
-                             dusty=basic_dusty,
                              correct_for_molecular_absorption=args.molecular_absorption,
                              molecular_absorption_lookup_table_path=args.molecular_table_path,
                              limits_only=args.limits_only,
