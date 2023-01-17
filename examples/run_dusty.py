1813ac00da0d279ca1ca8a582deacc588cf7af5a
viraj21197 <karambelkarvraj21197@gmail.com>
2023-01-17 13:40:08 -0800

    add example to run only dusty

---
+++ examples/run_dusty.py
@@ -0,0 +1,67 @@
+from pydusty.dusty import Dusty, DustyParameters
+from pydusty.parameters import Parameter
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tstar", type=float, default=5000)
+    parser.add_argument("--tau", type=float, default=50)
+    parser.add_argument("--tdust", type=float, default=1000)
+    parser.add_argument("--thick", type=float, default=2.0)
+    parser.add_argument("--dtype", choices=['graphitic', 'silicate'], default='graphitic')
+    parser.add_argument('workdir', type=str, default=None, help='dusty workdir name')
+    parser.add_argument('--dusty_file_dir', type=str, default='data/dusty_files',
+                        help='Directory with dusty code files')
+
+    args = parser.parse_args()
+
+    tstar = Parameter(name='tstar',
+                      value=args.tstar,
+                      is_variable=False)
+
+    tdust = Parameter(name='tdust',
+                      value=args.tdust,
+                      is_variable=True)
+
+    tau = Parameter(name='tau',
+                    value=args.tau,
+                    is_variable=False)
+
+    blackbody = Parameter(name='blackbody',
+                          value=True)
+    shell_thickness = Parameter(name='shell_thickness',
+                                value=args.thick)
+    dust_type = Parameter(name='dust_type',
+                          value=args.dtype)
+    tstarmin = Parameter(name='tstarmin',
+                         value=3500)
+    tstarmax = Parameter(name='tstarmin',
+                         value=48999)
+    custom_grain_distribution = Parameter(name='custom_grain_distribution',
+                                          value=False)
+
+    dusty_parameters = DustyParameters(
+        tstar=tstar,
+        tdust=tdust,
+        tau=tau,
+        blackbody=blackbody,
+        shell_thickness=shell_thickness,
+        dust_type=dust_type,
+        tstarmin=tstarmin,
+        tstarmax=tstarmax,
+        custom_grain_distribution=custom_grain_distribution
+    )
+
+    dusty_runner = Dusty(parameters=dusty_parameters,
+                         dusty_working_directory=args.workdir,
+                         dusty_file_directory=args.dusty_file_dir
+                         )
+    dusty_runner.generate_input()
+    dusty_runner.run()
+
+    lam, flx, npt, r1, ierror = dusty_runner.get_results()
+    with open(f'{args.workdir}/sed.dat','w') as f:
+        f.write("lam, flux\n")
+        for ind in range(len(lam)):
+            f.write(f"{lam[ind]}, {flx[ind]}\n")
+            
