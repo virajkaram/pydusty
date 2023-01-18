6439537bed913f0e5e79e380da33f227ebdf766e
viraj21197 <karambelkarvraj21197@gmail.com>
2023-01-17 21:34:45 -0800

    add r1 to dusty output spec

--- examples/run_dusty.py
+++ examples/run_dusty.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
 
     lam, flx, npt, r1, ierror = dusty_runner.get_results()
     with open(f'{args.workdir}/sed_{tstar.value}_{tdust.value}_{tau.value}_{dust_type.value}.dat','w') as f:
+        f.write(f"# {r1}\n")
         f.write("lam, flux\n")
         for ind in range(len(lam)):
             f.write(f"{lam[ind]}, {flx[ind]}\n")
