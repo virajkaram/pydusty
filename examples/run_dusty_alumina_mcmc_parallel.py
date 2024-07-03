1ba667a81dca8a268aed0e3958f4a0603b0fbb29
viraj21197 <karambelkarvraj21197@gmail.com>
2024-07-03 07:46:12 -0700

    alumina mcmc

---
+++ examples/run_dusty_alumina_mcmc_parallel.py
@@ -0,0 +1,101 @@
+from pydusty.dusty import DustyParameters, Dusty_Alumina
+from pydusty.mcmc import Emcee
+from pydusty.parameters import Parameter
+from pydusty.priors import UniformPrior
+from pydusty.utils import get_default_argparser, load_and_extcor_data, getLogger
+from pydusty.parallel import ParallelEmceeRunner
+
+
+def run_mcmc_process(emcee_runner: Emcee):
+    emcee_runner.run_mcmc()
+
+
+if __name__ == '__main__':
+
+    argparser = get_default_argparser()
+    args = argparser.parse_args()
+    logger = getLogger(args.loglevel, args.logfile)
+
+    # Set initial parameters
+    tstar = Parameter(name='tstar',
+                      value=5000,
+                      is_variable=False)
+
+    tdust = Parameter(name='tdust',
+                      value=1100,
+                      is_variable=False)
+
+    tau = Parameter(name='tau',
+                    value=1,
+                    prior=UniformPrior(prior_parameters=(1e-3, 7)),
+                    is_variable=True)
+
+    tau_wav_micron = Parameter(name='tau_wav', value=100,
+                               is_variable=False)
+
+    blackbody = Parameter(name='blackbody',
+                          value=True)
+
+    shell_thickness = Parameter(name='shell_thickness',
+                                value=2.0)
+
+    dust_type = Parameter(name='dust_type',
+                          value=f'si_{args.si}_al_{args.al}_'
+                                f'{args.al_type}_tau_{args.tau_wav_micron}um')
+
+    tstarmin = Parameter(name='tstarmin',
+                         value=3500)
+
+    tstarmax = Parameter(name='tstarmin',
+                         value=48999)
+
+    custom_grain_distribution = Parameter(name='custom_grain_distribution',
+                                          value=False)
+
+    si_abundance = Parameter(name='si', value=0.5,
+                             prior=UniformPrior(prior_parameters=(0,1)),
+                             is_variable=True)
+
+    al_abundance = Parameter(name='al', value=0.5,
+                             prior=UniformPrior(prior_parameters=(0,1)),
+                             is_variable=True)
+
+    dusty_parameters = DustyParameters(
+        tstar=tstar,
+        tdust=tdust,
+        tau=tau,
+        blackbody=blackbody,
+        shell_thickness=shell_thickness,
+        dust_type=dust_type,
+        tstarmin=tstarmin,
+        tstarmax=tstarmax,
+        custom_grain_distribution=custom_grain_distribution,
+        tau_wavelength_microns=tau_wav_micron,
+        si_dl_abundance=si_abundance,
+        al_com_abundance=al_abundance,
+    )
+
+    working_dir = args.workdir
+
+    ext_corrected_obsdata = load_and_extcor_data(args.object_photometry_file)
+
+    parallel_emcee_runner = ParallelEmceeRunner(dusty=Dusty_Alumina,
+                                                dusty_parameters=dusty_parameters,
+                                                nwalkers=args.nwalkers,
+                                                working_dir=working_dir,
+                                                obsdata=ext_corrected_obsdata,
+                                                object_photometry_file=args.object_photometry_file,
+                                                nprocesses=args.nprocesses,
+                                                ntrials=args.ntrials,
+                                                correct_for_molecular_absorption=args.molecular_absorption,
+                                                molecular_absorption_lookup_table_path=args.molecular_table_path,
+                                                limits_only=args.limits_only,
+                                                fixLstar=args.fixLstar,
+                                                chi_square_limits_only=args.chi_square_limits_only,
+                                                extrapolation=args.extrapolation,
+                                                continue_from_file=args.continue_from_file,
+                                                dusty_file_dir=args.dusty_file_dir,
+                                                )
+    parallel_emcee_runner.run()
+    parallel_emcee_runner.make_plots()
+    emcee_runners = []
