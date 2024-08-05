3e0623488abb1877fc9de8c71e60267bf50d9572
viraj21197 <karambelkarvraj21197@gmail.com>
2024-08-04 19:59:33 -0700

    multiple components

---
+++ examples/run_dusty_two_comp_dust_silicate.py
@@ -0,0 +1,93 @@
+import os
+
+from pydusty.dusty import Dusty_Two_Component_Silicate, DustyParameters
+from pydusty.parameters import Parameter
+import argparse
+from pydusty.utils import getLogger
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tstar", type=float, default=5000)
+    parser.add_argument("--tau", type=float, default=50)
+    parser.add_argument("--tau_wav_micron", type=float, default=100.0,
+                        help="wavelength in um at which tau is specified")
+    parser.add_argument("--tdust", type=float, default=1000)
+    parser.add_argument("--thick", type=float, default=2.0)
+    parser.add_argument("--dtype", choices=['alumina',
+                                            'crystaline_silicate',
+                                            'glassy_silicate',
+                                            'glassy_olivine',
+                                            'glassy_pyroxene',
+                                            'foe'
+                                            ],
+                        )
+    parser.add_argument('--sil_abundance', type=float, default=0.5)
+    parser.add_argument('workdir', type=str, default=None, help='dusty workdir name')
+    parser.add_argument('--dusty_file_dir', type=str, default='data/dusty_files',
+                        help='Directory with dusty code files')
+    parser.add_argument('--loglevel', type=str, default='DEBUG', help='logging level')
+    parser.add_argument('--logfile', type=str, default=None, help='log file')
+
+    args = parser.parse_args()
+
+    logger = getLogger(args.loglevel, args.logfile)
+
+    tstar = Parameter(name='tstar',
+                      value=args.tstar,
+                      is_variable=False)
+
+    tdust = Parameter(name='tdust',
+                      value=args.tdust,
+                      is_variable=True)
+
+    tau = Parameter(name='tau',
+                    value=args.tau,
+                    is_variable=False)
+
+    blackbody = Parameter(name='blackbody',
+                          value=True)
+    shell_thickness = Parameter(name='shell_thickness',
+                                value=args.thick)
+    dust_type = Parameter(name='dust_type',
+                          value=args.dtype)
+    tstarmin = Parameter(name='tstarmin',
+                         value=3500)
+    tstarmax = Parameter(name='tstarmin',
+                         value=48999)
+    custom_grain_distribution = Parameter(name='custom_grain_distribution',
+                                          value=False)
+    tau_wav_micron = Parameter(name='tau_wav', value=args.tau_wav_micron,
+                               is_variable=False)
+
+    second_comp_abundance = Parameter(name=args.dtype, value=1 - args.sil_abundance,
+                                      is_variable=False)
+
+    dusty_parameters = DustyParameters(
+        tstar=tstar,
+        tdust=tdust,
+        tau=tau,
+        blackbody=blackbody,
+        shell_thickness=shell_thickness,
+        dust_type=dust_type,
+        tstarmin=tstarmin,
+        tstarmax=tstarmax,
+        custom_grain_distribution=custom_grain_distribution,
+        tau_wavelength_microns=tau_wav_micron,
+        al_com_abundance=second_comp_abundance,
+    )
+
+    dusty_runner = Dusty_Two_Component_Silicate(parameters=dusty_parameters,
+                                 dusty_working_directory=args.workdir,
+                                 dusty_file_directory=args.dusty_file_dir
+                                 )
+
+    os.chdir(args.workdir)
+    dusty_runner.generate_input()
+    dusty_runner.run()
+
+    lam, flx, npt, r1, ierror = dusty_runner.get_results()
+    with open(f'{args.workdir}/sed_{tstar.value}_{tdust.value}_{tau.value}_{dust_type.value}_{shell_thickness.value}.dat', 'w') as f:
+        f.write(f"# {r1}\n")
+        f.write("lam, flux\n")
+        for ind in range(len(lam)):
+            f.write(f"{lam[ind]}, {flx[ind]}\n")
--- pydusty/dusty.py
+++ pydusty/dusty.py
@@ -202,3 +202,65 @@ class Dusty_Alumina(BaseDusty):
         output.write('- visibility function at spec. wavelengths; fname.v### = 0  \n')
         output.close()
         logger.info(f"Writing dusty file with {self.parameters.get_printable_string()}")
+
+
+class Dusty_Two_Component_Silicate(BaseDusty):
+
+    def generate_input(self):
+        assert self.parameters.al_com_abundance is not None, "Alumina abundance must be specified"
+        output = open(f'{self.file_basename}.inp', 'w')
+
+        if self.parameters.al_com_abundance.name == 'alumina':
+            dust_comp_file = 'Al2O3-comp.nk'
+        elif self.parameters.al_com_abundance.name == 'crystaline_silicate':
+            dust_comp_file = 'crSil.nk'
+        elif self.parameters.al_com_abundance.name == 'glassy_olivine':
+            dust_comp_file = 'gloliMg50.nk'
+        elif self.parameters.al_com_abundance.name == 'glassy_pyroxene':
+            dust_comp_file = 'glpyrMg50.nk'
+        elif self.parameters.al_com_abundance.name == 'feo':
+            dust_comp_file = 'FeO.nk'
+        else:
+            raise ValueError(f'Unknown dust component {self.parameters.al_com_abundance.name}')
+        if (self.parameters.tstar.value < self.parameters.tstarmin.value or self.parameters.tstar.value > self.parameters.tstarmax.value) or (
+        self.parameters.blackbody.value):
+            output.write('Spectrum = 1\n')
+            output.write('Number of BB = 1\n')
+            output.write(f'Temperature = {round(self.parameters.tstar.value, 2)}\n')
+        else:
+            output.write('Spectrum = 5   \n')
+            output.write('\n')
+        output.write('   optical properties index = 2 \n')
+        output.write('Abundances for supported grain types:\n')
+        output.write('   #   Sil-Ow  Sil-Oc  Sil-DL  grf-DL  amC-Hn  SiC-Pg \n')
+        output.write(f'    x = 0.00    0.00   {1 - self.parameters.al_com_abundance.value}    0.00    0.00    0.00 \n')
+        output.write('Number of additional components = 1, properties listed in:\n')
+        output.write(f'{dust_comp_file}\n')
+        output.write(f'Abundances for these components = {self.parameters.al_com_abundance.value} \n')
+        if self.parameters.custom_grain_distribution.value:
+            output.write('- size distribution = 2  % custom       \n')
+            output.write(f'  q = 3.5, a(min) = {self.parameters.min_grain_size.value} micron, a(max) = {self.parameters.max_grain_size.value} micron\n')
+        else:
+            output.write('- size distribution = 1  % standard MRN    \n')
+        output.write(f'- temperature = {self.parameters.tdust.value} K \n')
+        output.write('- density type = 1                   \n')
+        output.write('- number of powers = 1              \n')
+        output.write(f'- shells relative thickness = {self.parameters.shell_thickness.value}\n')
+        output.write('- power = 2 \n')
+        output.write('- grid type = 1                  % linear grid \n')
+        output.write(f'- lambda0 = {self.parameters.tau_wavelength_microns.value} micron          % optical depth specified  \n')
+        output.write('- tau(min) = ' + str(
+            self.parameters.tau.value) + ' ; tau(max) = 1000.0   % for the visual wavelength \n')
+        output.write('- number of models = 1           \n')
+        output.write('- accuracy for flux conservation = 0.05             \n')
+        output.write('- verbosity flag;                              verbose = 1  \n')
+        output.write('- properties of emerging spectra;            fname.spp = 1  \n')
+        output.write('- detailed spectra for each model;          fname.s### = 1  \n')
+        output.write('- images at specified wavelengths;          fname.i### = 1  \n')
+        output.write('     number of wavelengths = 5  \n')
+        output.write('     wavelengths = 3.5, 4.5, 6.0, 8.0, 24.0 micron  \n')
+        output.write('- radial profiles for each model;           fname.r### = 1  \n')
+        output.write('- detailed run-time messages;               fname.m### = 1  \n')
+        output.write('- visibility function at spec. wavelengths; fname.v### = 0  \n')
+        output.close()
+        logger.info(f"Writing dusty file with {self.parameters.get_printable_string()}")
