e95b0a98c08a4b9b0b1237c1315338014eebaae6
viraj21197 <karambelkarvraj21197@gmail.com>
2024-12-26 11:43:11 -0800

    diff params

--- examples/make_dusty_model_grid.py
+++ examples/make_dusty_model_grid.py
@@ -26,12 +26,11 @@ if __name__ == '__main__':
 
     logger = getLogger(args.loglevel, args.logfile)
 
-    tstar_values = [1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500]
+    # tstar_values = [1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500]
+    tstar_values = [3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000, 6250, 6500,]
     tdust_values = [500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100,
                     1150, 1200, 1250, 1300, 1350, 1400, 1450, 1500]
-    # tau_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11,
-    #               0.12, 0.13, 0.14, 0.15]
-    tau_values = 10**np.linspace(0, 1, 7)
+    tau_values = 10**np.linspace(-2, 1, 15)
     blackbody = Parameter(name='blackbody',
                           value=True)
     shell_thickness = Parameter(name='shell_thickness',
