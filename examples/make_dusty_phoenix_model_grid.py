90980371deded5915d0535c026e868b67ebf59e8
viraj21197 <karambelkarvraj21197@gmail.com>
2025-01-31 20:47:40 -0800

    fix

--- examples/make_dusty_phoenix_model_grid.py
+++ examples/make_dusty_phoenix_model_grid.py
@@ -14,7 +14,7 @@ from astropy.convolution import Box1DKernel, convolve
 def convert_phoenix_file_to_dusty_format(phoenix_filename, outfilename, log_gkey='g00'):
     phnx = Table.read(phoenix_filename)
     # Restrict to wavelengths between 0.1 and 20 microns
-    phnx['wav_um'] = phnx['WAVELENGTH'].values / 1e4
+    phnx['wav_um'] = phnx['WAVELENGTH'] / 1e4
     phnx = phnx[(phnx['wav_um']>0.1) & (phnx['wav_um']<20)]
     phnx_flx = phnx[log_gkey].values
     phnx_wavs = phnx['wav_um']
