04446dddcb4a8189a81413c09ff864919b66d4e3
viraj21197 <karambelkarvraj21197@gmail.com>
2025-03-28 15:21:32 -0700

    more Tstars

--- examples/make_dusty_model_grid_sioliv_ratios.py
+++ examples/make_dusty_model_grid_sioliv_ratios.py
@@ -22,7 +22,8 @@ if __name__ == '__main__':
 
     logger = getLogger(args.loglevel, args.logfile)
 
-    tstar_values = [2000, 2500, 3000, 3500, 4000, 4500]
+    # tstar_values = [2000, 2500, 3000, 3500, 4000, 4500]
+    tstar_values = [5000, 5500]
     tdust_values = [100, 200, 300, 400, 500, 600]
     olivine_abundance_values = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
     tau_values = [50, 70, 90, 110, 130, 150]
