40b48bc0b356b3b1b0435257950e22dd65243a5a
viraj21197 <karambelkarvraj21197@gmail.com>
2025-03-28 10:45:24 -0700

    make glassy olivine grid

---
+++ examples/make_dusty_model_grid_sioliv_ratios.py
@@ -0,0 +1,98 @@
+import os
+
+from pydusty.dusty import DustyParameters, Dusty_Two_Component_Silicate
+from pydusty.parameters import Parameter
+import argparse
+from pydusty.utils import getLogger
+from pathlib import Path
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tau_wav_micron", type=float, default=0.55,
+                        help="wavelength in um at which tau is specified")
+    parser.add_argument("--thick", type=float, default=2.0)
+    parser.add_argument('workdir', type=str, default=None, help='dusty workdir name')
+    parser.add_argument('--dusty_file_dir', type=str, default='/scr2/viraj/dusty/',
+                        help='Directory with dusty code files')
+    parser.add_argument('--loglevel', type=str, default='DEBUG', help='logging level')
+    parser.add_argument('--logfile', type=str, default=None, help='log file')
+
+    args = parser.parse_args()
+
+    logger = getLogger(args.loglevel, args.logfile)
+
+    tstar_values = [2000, 2500, 3000, 3500, 4000, 4500]
+    tdust_values = [100, 200, 300, 400, 500, 600]
+    olivine_abundances = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    tau_values = [50, 70, 90, 110, 130, 150]
+    blackbody = Parameter(name='blackbody',
+                          value=True)
+    shell_thickness = Parameter(name='shell_thickness',
+                                value=args.thick)
+    tstarmin = Parameter(name='tstarmin',
+                         value=3500)
+    tstarmax = Parameter(name='tstarmin',
+                         value=48999)
+    custom_grain_distribution = Parameter(name='custom_grain_distribution',
+                                          value=False)
+    tau_wav_micron = Parameter(name='tau_wav', value=args.tau_wav_micron,
+                               is_variable=False)
+
+    workdir = args.workdir + f'/si_olivine_grid_thick_{shell_thickness.value}'
+    Path(workdir).mkdir(parents=True, exist_ok=True)
+    for tstarval in tstar_values:
+        for tdustval in tdust_values:
+            for tauval in tau_values:
+                for olval in olivine_abundances:
+                    dust_type = Parameter(name='dust_type',
+                                          value=f'si_{round(1 - olval, 2)}_oliv_{olval}_'
+                                                f'glassy_tau_{args.tau_wav_micron}um')
+                    olivine_abundances = Parameter(name='glassy_olivine',
+                                                   value=olval,
+                                                   is_variable=False)
+
+                    tstar = Parameter(name='tstar',
+                                      value=tstarval,
+                                      is_variable=False)
+
+                    tdust = Parameter(name='tdust',
+                                      value=tdustval,
+                                      is_variable=True)
+
+                    tau = Parameter(name=f'tau',
+                                    value=tauval,
+                                    is_variable=False)
+
+                    dusty_parameters = DustyParameters(
+                        tstar=tstar,
+                        tdust=tdust,
+                        tau=tau,
+                        blackbody=blackbody,
+                        shell_thickness=shell_thickness,
+                        dust_type=dust_type,
+                        tstarmin=tstarmin,
+                        tstarmax=tstarmax,
+                        custom_grain_distribution=custom_grain_distribution,
+                        tau_wavelength_microns=tau_wav_micron,
+                        al_com_abundance=olivine_abundances,
+                    )
+
+                    dusty_runner = Dusty_Two_Component_Silicate(
+                        parameters=dusty_parameters,
+                        dusty_working_directory=args.workdir,
+                        dusty_file_directory=args.dusty_file_dir
+                        )
+
+                    os.chdir(workdir)
+                    dusty_runner.generate_input()
+                    dusty_runner.run()
+
+                    lam, flx, npt, r1, ierror = dusty_runner.get_results()
+                    with open(
+                            f'{workdir}/sed_{tstar.value}_{tdust.value}_{tau.value}_{dust_type.value}_{shell_thickness.value}.dat',
+                            'w') as f:
+                        f.write(f"# {r1}\n")
+                        f.write("lam, flux\n")
+                        for ind in range(len(lam)):
+                            f.write(f"{lam[ind]}, {flx[ind]}\n")
